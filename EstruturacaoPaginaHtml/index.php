43039bdbe4a255ccaa70b3a5c63f22726980d14e
Lucas Schroeder <106642825+lucaoinovation@users.noreply.github.com>
2022-09-21 16:10:37 -0300

    Estruturação Inicial de Página Web
    Utilizando Php, Html e Css.

---
+++ EstruturacaoPaginaHtml/index.php
@@ -0,0 +1,20 @@
+<?php 
+    include __DIR__. 'footer.php';
+    include __DIR__. 'header.php';
+?>
+
+<html>
+    <body>
+        <div class="">
+            <h1>Bem-vindo ao mundo dos testes!</h1>
+            <button class="btn btn-success">
+                Ativar
+            </button>
+        </div>
+        <div class="form-group">
+            <form>
+                <a href="">Teste 01</a>
+            </form>
+        </div>
+    </body>
+</html>
